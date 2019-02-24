0796274eeef6661dd8a6220d13b3733cbe65907a
Zilog80 <rs1729@users.noreply.github.com>
2019-02-24 13:35:10 +0100

    rs41dm: json output, no numSV filter

--- demod/rs41dm_dft.c
+++ demod/rs41dm_dft.c
@@ -1104,7 +1104,7 @@ int print_position(int ec) {
 
         if (option_json) {
             // Print JSON output required by auto_rx.
-            if (!err && !err1 && !err3 && gpx.numSV >= 4) { // frame-nb/id && gps-time && gps-position  (crc-)ok; 3 CRCs, RS not needed
+            if (!err && !err1 && !err3) { // frame-nb/id && gps-time && gps-position  (crc-)ok; 3 CRCs, RS not needed
                 if (option_ptu && !err0 && gpx.T > -273.0) {
                     printf("{ \"frame\": %d, \"id\": \"%s\", \"datetime\": \"%04d-%02d-%02dT%02d:%02d:%06.3fZ\", \"lat\": %.5f, \"lon\": %.5f, \"alt\": %.5f, \"vel_h\": %.5f, \"heading\": %.5f, \"vel_v\": %.5f, \"sats\": %d, \"temp\":%.1f }\n",  gpx.frnr, gpx.id, gpx.jahr, gpx.monat, gpx.tag, gpx.std, gpx.min, gpx.sek, gpx.lat, gpx.lon, gpx.alt, gpx.vH, gpx.vD, gpx.vU, gpx.numSV, gpx.T );
                 } else {
