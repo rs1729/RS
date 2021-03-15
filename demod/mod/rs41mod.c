b8815d7ff76567f56ec671ca505564f4f942950a
Zilog80 <rs1729@users.noreply.github.com>
2021-03-15 01:31:49 +0100

    rs41mod: bugfix json silent mode

--- demod/mod/rs41mod.c
+++ demod/mod/rs41mod.c
@@ -1789,9 +1789,9 @@ static int print_position(gpx_t *gpx, int ec) {
 
             if ( pos > frm_end )  // end of (sub)frame
             {
-                if (gpx->option.ptu && out && !sat && !encrypted && pck_ptu > 0) {
+                if (gpx->option.ptu && !sat && !encrypted && pck_ptu > 0) {
                     err0 = get_PTU(gpx, ofs_ptu, pck_ptu, !err3);
-                    if (!err0) prn_ptu(gpx);
+                    if (!err0 && out) prn_ptu(gpx);
                 }
                 pck_ptu = 0;
 
@@ -2363,9 +2363,13 @@ int main(int argc, char *argv[]) {
                     }
                     if ( bitQ == EOF ) break; // liest 2x EOF
 
-                    softbits[b8pos] = hsbit.sb;
+                    if (gpx.option.inv) {
+                        bit ^= 1;
+                        hsbit.hb ^= 1;
+                        hsbit.sb = -hsbit.sb; // does not affect ecc3
+                    }
 
-                    if (gpx.option.inv) bit ^= 1;
+                    softbits[b8pos] = hsbit.sb;
 
                     bitpos += 1;
                     bitbuf[b8pos] = bit;
