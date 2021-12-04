bbf295414b917774278088e534d114bdd4a9af57
Pavol Gajdoš <pavol1gajdos@gmail.com>
2021-12-04 19:42:33 +0100

    scripts - json

--- demod/mod/win-script/rs92mod-raw.bat
+++ demod/mod/win-script/rs92mod-raw.bat
@@ -1,3 +1,3 @@
 rs92mod.exe --ecc2 -vv --rawhex %* > rs92_data_all.txt 
 rs92mod.exe --ecc2 --rawhex %* > rs92_data.txt 
-rs92mod.exe --ecc2 --json --rawhex %* > rs92_data.json 
+rs92mod.exe --ecc2 --json --rawhex --silent %* > rs92_data.json 
