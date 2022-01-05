18e98e0b5d69b23e3317eb063ec0d7e0a3315540
Pavol Gajdoš <pavol1gajdos@gmail.com>
2022-01-05 17:01:59 +0100

    rs41 scripts

--- demod/mod/linux-script/rs41mod-raw.sh
+++ demod/mod/linux-script/rs41mod-raw.sh
@@ -1,4 +1,8 @@
-./rs41mod --ecc4 -vv --ptu2 --dewp --rawhex "$@" > rs41_data_all.txt 
-./rs41mod --ecc4 --ptu2 --dewp --rawhex "$@" > rs41_data.txt 
-./rs41mod --ecc4 --sat --rawhex "$@" > rs41_data-sat.txt 
-./rs41mod --ecc4 --ptu2 --dewp --json --rawhex --silent "$@" > rs41_data.json 
+cp "$1" "$1""a"
+cat "$1" >> "$1""a"
+./rs41mod --ecc4 -vv --ptu2 --dewp --rawhex "$1" > "$1"_data_all.txt 
+./rs41mod --ecc4 --ptu2 --dewp --rawhex "$1" > "$1"_data.txt 
+./rs41mod --ecc4 --sat --rawhex "$1" > "$1"_data-sat.txt 
+./rs41mod --ecc4 --ptu2 --dewp --json --silent --rawhex "$1" > "$1"_data.json
+./rs41mod --ecc4 --ptu2 --dewp --json --silent --rawhex "$1""a" > "$1"_data1.json 
+rm "$1""a"
--- demod/mod/win-script/rs41mod-raw.bat
+++ demod/mod/win-script/rs41mod-raw.bat
@@ -1,4 +1,8 @@
-rs41mod.exe --ecc4 -vv --ptu2 --dewp --rawhex %* > rs41_data_all.txt 
-rs41mod.exe --ecc4 --ptu2 --dewp --rawhex %* > rs41_data.txt 
-rs41mod.exe --ecc4 --sat --rawhex %* > rs41_data-sat.txt 
-rs41mod.exe --ecc4 --ptu2 --dewp --json --rawhex --silent %* > rs41_data.json 
+copy %1 %1"a"
+type %1 >> %1"a"
+rs41mod.exe -vv --ptu2 --dewp --rawhex --ecc4 %1 > %1_data_all.txt 
+rs41mod.exe --ptu2 --dewp --rawhex --ecc4 %1 > %1_data.txt 
+rs41mod.exe --sat --rawhex --ecc4 %1 > %1_data-sat.txt 
+rs41mod.exe --ptu2 --dewp --json --silent --rawhex --ecc4 %1 > %1_data.json 
+rs41mod.exe --ptu2 --dewp --json --silent --rawhex --ecc4 %1"a" > %1_data1.json
+del %1"a"
